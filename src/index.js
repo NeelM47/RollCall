6ec20a0b83c829f58753ff6e3223af159f288cd6
NeelM47 <neel.more@postgrad.manchester.ac.uk>
2024-11-05 19:54:06 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', { 
+	useNewUrlParser: true, 
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('MongoDB connected successfully');
+}).catch(err => {
+	console.error('MongoDB connection error:', err);
+});
+
+const DataSchema = new mongoose.Schema({
+	name: String,
+	value: String
+});
+
+const Data = mongoose.model('Data', DataSchema);
+
+app.get('/data', async (req, res) => {
+	const data = await Data.find();
+	res.json(data);
+});
+
+app.post('/data', async (req, res) => {
+	const newData = new Data({ 
+		name: req.body.name,
+		value: req.body.value
+	});
+	try{
+		await newData.save();
+		console.log('Data saved successfully:', newData);
+		res.send('Data saved successfully');
+	} catch (error) {
+		console.error('Error saving data:', error);
+		res.status(500).send('Error saving data');
+	}
+});
+
+//app.get('/', (req, res) => {
+//    res.send('Hello, World!');
+//});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+
+});
+
